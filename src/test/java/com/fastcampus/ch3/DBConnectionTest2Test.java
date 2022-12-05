6dfbdf6d2fee0774f841250cd1807f539400646d
daye <gomdaye@gmail.com>
2022-12-05 22:13:04 +0900

    spring으로 db다루기 - tdd 연습

--- src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +21,130 @@ import static org.junit.Assert.*;
 public class DBConnectionTest2Test {
     @Autowired
     DataSource ds;
+
+    @Test
+    public void insertUserTest() throws Exception{
+        User user = new User("asdf13", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        deleteAll();
+        int rowCnt = insertUser(user);
+        assertTrue(rowCnt==1);
+    }
+
+    @Test
+    public void selectUserTest() throws Exception{
+        deleteAll();
+        User user = new User("asdf13", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        int rowCnt = insertUser(user);
+        User user2 = selectUser("asdf13");
+
+        assertTrue(user.getId().equals("asdf13"));
+    }
+
+    @Test
+    public void updateUserTest() throws Exception{
+        User user = new User("asdf13", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        int rowCnt = updateUser(user);
+        assertTrue(rowCnt==1);
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+        deleteAll();
+        int rowCnt = deleteUser("asfd");
+
+        assertTrue(rowCnt==0);
+
+        User user = new User("asdf13", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        rowCnt = insertUser(user);
+        assertTrue(rowCnt==1);
+
+        rowCnt = deleteUser(user.getId());
+        assertTrue(rowCnt==1);
+
+        assertTrue(selectUser(user.getId())==null);
+    }
+
+    //매개변수로 받은 사용자 정보로 user_info테이블을 update하는 메서드
+    public int updateUser(User user) throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "update user_info set pwd=?, email=? where id=?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); //SQL Injection공격, 성능향상
+        pstmt.setString(1, "1111");
+        pstmt.setString(2, "bbb@bbbb");
+        pstmt.setString(3, user.getId());
+
+        return pstmt.executeUpdate(); // insert, delete, update 일 때 사용
+    }
+
+    public int deleteUser(String id) throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info where id=?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); //SQL Injection공격, 성능향상
+        pstmt.setString(1, id);
+        return pstmt.executeUpdate(); // insert, delete, update 일 때 사용
+    }
+
+
+    public User selectUser(String id) throws Exception{
+        Connection conn = ds.getConnection();
+
+        String sql = "select * from user_info where id=?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); //SQL Injection공격, 성능향상
+        pstmt.setString(1, id);
+        ResultSet rs = pstmt.executeQuery(); // select 사용
+
+        if(rs.next()){
+            User user = new User();
+            user.setId(rs.getString(1));
+            user.setPwd(rs.getString(2));
+            user.setName(rs.getString(3));
+            user.setEmail(rs.getString(4));
+            user.setBirth(new Date(rs.getDate(5).getTime()));
+            user.setSns(rs.getString(6));
+            user.setReg_date(new Date(rs.getDate(7).getTime()));
+
+            return user;
+        }
+
+        return null;
+    }
+
+    private void deleteAll() throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); //SQL Injection공격, 성능향상
+        pstmt.executeUpdate(); // insert, delete, update 일 때 사용
+    }
+
+    // 사용자 정보를 user_info테이블에 저장하는 메서드
+    public int insertUser(User user) throws Exception{
+        Connection conn = ds.getConnection();
+
+//        insert into user_info (id, pwd, name, email, birth, sns, reg_date)
+//        values ('asdf', '1234', 'smith', 'aaa@aaa.com', '2022-12-03', 'facebook', now());
+
+        String sql = "insert into user_info values (?, ?, ?, ?, ?, ?, now())";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); //SQL Injection공격, 성능향상
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getPwd());
+        pstmt.setString(3, user.getName());
+        pstmt.setString(4, user.getEmail());
+        pstmt.setDate(5, new java.sql.Date(user.getBirth().getTime()));
+        pstmt.setString(6, user.getSns());
+
+        int rowCnt = pstmt.executeUpdate(); // insert, delete, update 일 때 사용
+
+
+        return rowCnt;
+    }
 
 
     @Test
